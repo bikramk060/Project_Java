b8d689152fbafc2a0c04ea81e58ea73277512bd4
bikramk060 <bikramk060@gmail.com>
2019-07-22 07:46:04 +0530

    Create file.java

---
+++ file.java
@@ -0,0 +1,7 @@
+public class Javasample {
+
+    public static void main(String[] args) throws InterruptedException {
+        Thread.sleep(2000);
+        System.out.println("Hello java);
+    }
+}
